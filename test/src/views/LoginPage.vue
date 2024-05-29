3142a54ac610844515997319d6ed377291a4fe01
TinaGu0 <144460946+TinaGu0@users.noreply.github.com>
2024-05-29 09:47:13 -0400

    a

--- test/src/views/LoginPage.vue
+++ test/src/views/LoginPage.vue
@@ -1,10 +1,36 @@
 <template>
-    <div>
-        <h1>hi</h1>
-    </div>
+    <form>
+        <div class="card flex justify-content-center">
+            <FloatLabel>
+                <InputText id="email" v-model="email" />
+                <label for="email">Email</label>
+            </FloatLabel>
+            <FloatLabel>
+                <InputText id="username" v-model="user" />
+                <label for="username">Username</label>
+            </FloatLabel>
+        </div>
+        <div class="card flex justify-content-center">
+            <FloatLabel>
+                <Password v-model="pass" toggleMask />
+                <label for="password">Password</label>
+            </FloatLabel>
+        </div>
+        <Button label="Submit" @click="submit()"/>
+    </form>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import Password from 'primevue/password';
+import FloatLabel from 'primevue/floatlabel';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
+
+const email = ref(null);
+const user = ref(null);
+const pass = ref(null);
 
 </script>
 
